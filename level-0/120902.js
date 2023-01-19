30af8eda1ae44bfcbc0ad43e4e32db8cff1b7e2d
minenam <nhm6947@gmail.com>
2023-01-20 00:01:50 +0900

    feat: 120902 풀이 추가

---
+++ level-0/120902.js
@@ -0,0 +1,18 @@
+function solution(my_string) {
+    // my_String을 공백으로 쪼개기  --> 구글링
+    let stringList = my_string.split(' '); // [ '3', '+', '4' ]
+
+    let total = Number(stringList[0]); // 3
+    
+    // 쪼갠 것들을 for문으로 돌려서 +인 경우, -인 경우로 계산하기
+    for (let i=0; i < stringList.length; i++) {
+
+        if (stringList[i] === "+") {
+           total += Number(stringList[i+1]);
+        } 
+        if (stringList[i] === "-") {
+            total -= Number(stringList[i+1]);
+        } 
+    }
+    return total;
+}
