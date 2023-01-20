754fe2de890c8deec6991e8cf36938204b0f7b50
kims331 <122257715+kims331@users.noreply.github.com>
2023-01-21 00:18:49 +0900

    feat:  풀이 추가

---
+++ level-0/kims331/120841.js
@@ -0,0 +1,14 @@
+function solution(dot) {
+    if(dot[0]>0 && dot[1]>0){
+        return 1;
+    }
+    if(dot[0]<0 && dot[1]>0){
+        return 2;
+    }
+    if(dot[0]<0 && dot[1]<0){
+        return 3;
+    }
+    if(dot[0]>0 && dot[1]<0){
+        return 4;
+}
+}
