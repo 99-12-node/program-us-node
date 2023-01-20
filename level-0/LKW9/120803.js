8eb51d092eea192efc15bbdae2f2c514be6380c6
LKW9 <92284361+LKW9@users.noreply.github.com>
2023-01-20 23:58:59 +0900

    feat: 0120 풀이 추가

---
+++ level-0/LKW9/120803.js
@@ -0,0 +1,7 @@
+function solution(num1, num2) {
+    let answer;
+    
+    answer = num1 -num2;
+    
+    return answer;
+}
---
+++ level-0/LKW9/120805.js
@@ -0,0 +1,10 @@
+function solution(num1, num2) {
+
+    let answer;
+    
+    answer = num1 / num2    
+    console.log(answer)
+    return Math.floor(answer);
+    
+    
+}
