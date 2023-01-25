992ee6cc6d455efad965141e66dc549968a580fe
seungwoon <seong3546@gmail.com>
2023-01-25 20:34:09 +0900

    Revert "feat: 120808, 120809, 120810, 120811, 120812 풀이 추가"
    This reverts commit 67b25b8c045d1706babb8e6c2950898a747d56bc.

--- level-0/leeseungwoon/120809.js
+++
@@ -1,9 +0,0 @@
-let numbers = [1, 2, 3, 4, 5];
-
-function solution(numbers) {
-  var answer = [];
-  for (let i = 0; i < numbers.length; i++) {
-    answer.push(numbers[i] * 2);
-  }
-  return answer;
-}
--- level-0/leeseungwoon/120811.js
+++
@@ -1,5 +0,0 @@
-function solution(array) {
-    array.sort((a,b) => a - b);
-    let val = parseInt(array.length/2);
-    return array[val];
-}
