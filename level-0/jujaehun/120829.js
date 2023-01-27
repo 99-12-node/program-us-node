0de4b927dea7b99e881c412ea0e7e32add23f3f9
jaehunju1996 <jjdong9108@gmail.com>
2023-01-27 19:06:57 +0900

    feat: 39번까지 문제풀이

---
+++ level-0/jujaehun/120829.js
@@ -0,0 +1,17 @@
+// 각도기
+
+function solution(angle) {
+    let answer = 0;
+    
+    if (angle == 0){
+        
+    } else if(angle < 90){
+        return answer = 1;
+    } else if(angle == 90) {
+        return answer = 2;
+    } else if(angle <180) {
+        return answer = 3;
+    } else if(angle == 180) {}
+        return answer = 4;
+    return answer;
+}
