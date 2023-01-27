8465ad6cbfc1e341684b494b56b854c325e823e2
yyeon111 <yyeon111@naver.com>
2023-01-27 22:59:55 +0900

    feat: 120823 수정

--- level-0/yeonkyoungpark/120823.js
+++ level-0/yeonkyoungpark/120823.js
@@ -9,9 +9,11 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
-    console.log(input[0]);
-    let rows = '';
-    for(let i = 1; i<= input[0]; i++){
-        console.log('*'.repeat(i));
+    let n = Number(input[0]);
+    
+let rows = '';
+for(let i = 1; i<= input[0]; i++){
+rows = '*'.repeat(i)
+console.log(rows);
     }
 });
