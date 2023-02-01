1b5cfa0ce1c164a4fc5d6db330d2420996684821
jaehunju1996 <jjdong9108@gmail.com>
2023-02-01 17:16:54 +0900

    feat: 50번까지 문제풀이추가

--- level-0/jujaehun/1.programmers/120834.js
+++ level-0/jujaehun/1.programmers/120834.js
@@ -1,3 +1,13 @@
 // 외계행성의 나이
 
-// 잘모르겠다.
+function solution(my_string, letter) {
+    // answer 문자열로 선언!
+    let answer = '';
+    for (let i = 0; i < my_string.length; i++){
+        if (my_string[i] !== letter) {
+            answer +=my_string[i]
+        }
+    }
+    return answer;
+    // answer를 반환
+}
